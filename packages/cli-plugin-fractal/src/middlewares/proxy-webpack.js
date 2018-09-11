72d1420042e749d62f56d75dd1d1227c7d138883
Romain Lenzotti <romain.lenzotti@valtech.fr>
2018-09-11 17:03:01 +0200

    fix(fractal): Resolve problem with windows paths for proxy-webpack

--- packages/cli-plugin-fractal/src/middlewares/proxy-webpack.js
+++ packages/cli-plugin-fractal/src/middlewares/proxy-webpack.js
@@ -2,13 +2,9 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = (config, { host, port }) =>
   proxy(
-    config.fractal.proxyPatterns.concat([
-      '**/sockjs-node/**',
-      '/**.hot-update.json',
-      '/**.hot-update.js',
-      '/**.hot-update.js.map',
-      'ws://**'
-    ]),
+    config.fractal.proxyPatterns
+      .concat(['**/sockjs-node/**', '/**.hot-update.json', '/**.hot-update.js', '/**.hot-update.js.map', 'ws://**'])
+      .map(s => s.replace(/\\/gi, '/')),
     {
       target: `http://${host || 'localhost'}:${port}`,
       ws: true,
