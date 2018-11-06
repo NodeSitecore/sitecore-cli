96a1ff995a25a9b3837ea831730f58749a9fa8ec
romain.lenzotti <rom.lenzotti@gmail.com>
2018-11-06 15:18:36 +0100

    feat(fractal): Add fractal copy task

--- packages/cli-plugin-fractal/src/fractal/index.js
+++ packages/cli-plugin-fractal/src/fractal/index.js
@@ -1,11 +1,39 @@
 /* eslint-disable global-require,import/no-dynamic-require */
 const execa = require('execa');
+const gulp = require('gulp');
+// const chalk = require('chalk');
+// const tap = require('gulp-tap');
+const debug = require('gulp-debug');
+const newer = require('gulp-newer');
 const fs = require('fs-extra');
 const log = require('fancy-log');
 const stripAnsi = require('strip-ansi');
 const createInstance = require('./create-instance');
 
+const toPromise = stream =>
+  new Promise((resolve, reject) =>
+    stream
+      .on('end', resolve)
+      .on('finish', resolve)
+      .on('error', reject)
+  );
+
 module.exports = {
+  async copy(config) {
+    const { copy = [] } = config.get('fractal');
+
+    const promises = copy.map(task => {
+      return toPromise(
+        gulp
+          .src(task.paths)
+          .pipe(newer(task.outputDir))
+          .pipe(debug({ title: 'Copying ' }))
+          .pipe(gulp.dest(task.outputDir))
+      );
+    });
+
+    return Promise.all(promises);
+  },
   /**
    *
    * @returns {*}
